0669b84735fc4fd82c444f9ce6855357c0981220
dheer meena <dheer1998meena@gmail.com>
2020-10-03 16:46:49 +0530

    [Dheer] Add . added UC2

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace SnakeLadder
         {
             Console.WriteLine("Welcome to Snake And Ladder Game ");
             const int startPosition = 0;
+            Random die = new Random();
+            int dieNum = die.Next(1, 7);
+            Console.WriteLine("Die Number is {0} ", dieNum);
         }
     }
 }
